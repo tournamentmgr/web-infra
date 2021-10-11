e5811b04ffaddc73b47c2bf2c7794f6cb2e7a17a
Dan Quackenbush <25692880+danquack@users.noreply.github.com>
2021-10-10 23:06:14 -0400

    :goal_net: fix event types

--- cloudfront_distribution.tf
+++ cloudfront_distribution.tf
@@ -28,7 +28,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       headers = ["Origin", "Access-Control-Request-Headers", "Accept-Encoding", "Access-Control-Request-Method", "user-agent"]
     }
     function_association {
-      event_type   = "origin-response"
+      event_type   = "viewer-response"
       function_arn = aws_cloudfront_function.hsts_protection.arn
     }
     dynamic "function_association" {
@@ -45,13 +45,14 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
         function_arn = aws_cloudfront_function.index_redirect.0.arn
       }
     }
-    dynamic "function_association" {
-      for_each = var.enable_prerender ? [1] : []
-      content {
-        event_type   = "origin-request"
-        function_arn = aws_cloudfront_function.prerender.0.arn
-      }
-    }
+    dynamic "lambda_function_association" {
+       for_each = var.enable_prerender ? [1] : []
+       content {
+         event_type = "origin-request"
+         lambda_arn = "${aws_lambda_function.prerender.0.arn}:${aws_lambda_function.prerender.0.version}"
+       }
+     }
+
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
--- functions/hsts.js
+++ functions/hsts.js
@@ -1,33 +1,28 @@
 function handler(event) {
-
     //Get contents of response
     const response = event.request;
     const headers = response.headers;
 
-    //Set new headers 
-    headers['strict-transport-security'] = [{
-        key: 'Strict-Transport-Security',
-        value: 'max-age=31536000; includeSubDomains; preload'
-    }];
-    headers['x-content-type-options'] = [{
-        key: 'X-Content-Type-Options',
+    // Set HTTP security headers
+    // Since JavaScript doesn't allow for hyphens in variable names, we use the dict["key"] notation 
+    headers['strict-transport-security'] = {
+        value: 'max-age=63072000; includeSubdomains; preload'
+    };
+    headers['content-security-policy'] = {
+        value: "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"
+    };
+    headers['x-content-type-options'] = {
         value: 'nosniff'
-    }];
-    headers['x-frame-options'] = [{
-        key: 'X-Frame-Options',
+    };
+    headers['x-frame-options'] = {
         value: 'DENY'
-    }];
-    headers['x-xss-protection'] = [{
-        key: 'X-XSS-Protection',
+    };
+    headers['x-xss-protection'] = {
         value: '1; mode=block'
-    }];
-    headers['referrer-policy'] = [{
-        key: 'Referrer-Policy',
-        value: 'same-origin'
-    }];
+    };
 
     //Return modified response
-    if (event.Records[0].cf.request.uri.endsWith('.gz')) {
+    if (event.request.uri.endsWith('.gz')) {
         headers['content-encoding'] = [{
             key: 'Content-Encoding',
             value: 'gzip'
