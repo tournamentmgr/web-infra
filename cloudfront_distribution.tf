c9b0323e8a639d731d943684367f2b55d1538fe8
Daniel Quackenbush <25692880+danquack@users.noreply.github.com>
2025-12-27 13:14:16 -0500

    update tls 1.2 to 2025

--- cloudfront_distribution.tf
+++ cloudfront_distribution.tf
@@ -111,7 +111,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   viewer_certificate {
     acm_certificate_arn      = "arn:aws:acm:us-east-1:${data.aws_caller_identity.current.account_id}:certificate/${var.certificate_id}"
     ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
+    minimum_protocol_version = "TLSv1.2_2025"
   }
 
   dynamic "custom_error_response" {
